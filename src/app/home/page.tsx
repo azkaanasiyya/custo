154e9462d3bef3753ee70ac85efd0dde17aac8cf
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-13 14:25:03 +0700

    feat: add Features-1 section to Home page

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,3 +1,4 @@
+import Features1 from "@/components/home/features-1";
 import HomeHero from "@/components/home/hero";
 import HomeStats from "@/components/home/stats";
 
@@ -6,6 +7,7 @@ export default function HomePage() {
     <div>
       <HomeHero />
       <HomeStats />
+      <Features1 />
     </div>
   );
 }
