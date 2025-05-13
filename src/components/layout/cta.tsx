156a82f0f51c755e3143638ed1483a056c4873fa
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-05-13 10:08:20 +0700

    feat(all): add cta section to all page

---
+++ src/components/layout/cta.tsx
@@ -0,0 +1,16 @@
+import Header from "@/components/common/header";
+import { Button } from "../ui/button";
+export default function CTA() {
+    return (
+        <div className="cta bg-[url('/background/bg-cta.png')] bg-no-repeat bg-center p-16 flex flex-col justify-center items-center">
+            <div className="container max-w-[67rem] py-10 flex flex-col gap-8 justify-center items-center">
+                <Header
+                    title="Start Growing with Custo Now"
+                    description="Take control of your sales pipeline, automate tasks, and build stronger customer relationships with ease."
+                    className="max-w-[34.4375rem] text-center"
+                />
+                <Button variant="primary" size="base">Start for Free</Button>
+            </div>
+        </div>
+    )
+}
