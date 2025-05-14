b145a0c1a9609dcce19e9e234b24e9edd8fea46e
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-14 13:36:14 +0700

    fix : correct navigation links and blog detail routing

--- src/app/blogs/[slug]/page.tsx
+++ src/app/blogs/[slug]/page.tsx
@@ -1,13 +1,10 @@
+import HeaderBlogDetail from "@/components/blog/blogdetail/header";
+
 export default function Blogs() {
   return (
     <div className="flex w-full justify-center">
       <div className="flex flex-col gap-[3rem] w-full max-w-[67rem] py-[6.5rem]">
-        <h1 className="text-xlarge text-grayscale-950">
-          Blogs
-        </h1>
-        <p className="text-large text-grayscale-600">
-          Stay updated with the latest news, tips, and insights from the world of CRM and sales.
-        </p>
+       <HeaderBlogDetail />
       </div>
     </div>
   );
--- src/components/blog/blogdetail/header.tsx
+++ src/components/blog/blogdetail/header.tsx
@@ -6,7 +6,7 @@ export default function HeaderBlogDetail() {
     <div className="flex w-full justify-center">
       <div className="flex flex-col gap-[3rem] w-full max-w-[67rem] py-[6.5rem]">
         <div className="">
-          <Link href="#">
+          <Link href="/blogs">
             <ArrowLeft />
           </Link>
         </div>
