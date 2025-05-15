b2971ed20de518e0c4c1cfc9230dab15fc7625df
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-05-15 09:27:39 +0700

    feat(contact): add testimonial section to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,9 +1,11 @@
+import Testimonial from "@/components/common/testimonial";
 import ContactHero from "@/components/contact/hero";
 
 export default function ContactPage() {
     return (
         <div>
             <ContactHero />
+            <Testimonial />
         </div>
     )
 }
