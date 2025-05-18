ae105b6a548ada400a56c3c130c3dae4d78dbe42
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-05-18 22:48:29 +0700

    fix(footer): adjust responsive sizing and spacing on mobile

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -6,12 +6,16 @@ interface HeaderProps {
 
 export default function Header({ title, description, className = "" }: HeaderProps) {
     return (
-        <div className={`flex flex-col justify-center items-center gap-[1rem] ${className}`}>
+        <div className={`flex flex-col justify-center items-center gap-4 ${className}`}>
             <div className="typography font-semibold text-grayscale-950">
-                <h3>{title}</h3>
+                <h3 className="text-[28px] leading-[140%] md:text-[40px] md:leading-[120%]">
+                    {title}
+                </h3>
             </div>
             {description && (
-                <p className="text-xlarge text-grayscale-600">{description}</p>
+                <p className="text-[16px] leading-[160%] text-grayscale-600 md:text-xlarge">
+                    {description}
+                </p>
             )}
         </div>
     )
