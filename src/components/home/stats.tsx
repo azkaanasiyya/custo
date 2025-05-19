ea91f7943c2a981ce08ae6802512ec084df0fd06
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-19 11:30:00 +0700

    fix(stats) : improve responsiveness of stats section on Home page

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -3,10 +3,10 @@ import { stats } from "../data/stats";
 export default function HomeStats() {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-row gap-[3rem] w-full max-w-mobile md:max-w-tablet lg:max-w-desktop py-[4rem]">
+      <div className="flex flex-col md:flex-row md:gap-[1.25rem] lg:gap-[3rem] w-full max-w-mobile md:max-w-tablet lg:max-w-desktop py-[1.25rem] md:py-[4rem]">
         {stats.map((stat, index) => (
           <div
-            className="flex flex-col gap-[1.25rem] items-center py-[2.5rem]"
+            className="flex flex-col gap-[1.25rem] items-center py-[1.25rem] md:py-0"
             key={index}
           >
             <h2 className="font-sans font-semibold leading-[140%] text-[1.75rem] md:text-[2.25rem] lg:text-[2.5rem] text-primary-500">
@@ -16,7 +16,7 @@ export default function HomeStats() {
               <h4 className="font-sans font-semibold leading-[140%] md:text-[1.25rem] lg:text-[1.5rem]">
                 {stat.title}
               </h4>
-              <p className="text-m-regular text-[] md:text-[0.875rem] text-grayscale-600 text-center">
+              <p className="text-m-regular text-[0.75rem] md:text-[0.875rem] text-grayscale-600 text-center">
                 {stat.description}
               </p>
             </div>
