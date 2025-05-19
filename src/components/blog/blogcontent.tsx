609a444f379299e16583f899e73dcbe810c19af8
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-19 16:11:59 +0700

    fix(blog) : improve responsive layout of blog menu

--- src/components/blog/blogcontent.tsx
+++ src/components/blog/blogcontent.tsx
@@ -3,12 +3,12 @@ import BlogList from "../common/bloglist";
 export default function BlogContent() {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-col gap-[4rem] w-full max-w-[67rem] py-[6.5rem]">
+      <div className="flex flex-col gap-[4rem] w-full max-w-mobile md:max-w-tablet lg:max-w-desktop py-[6.5rem]">
         <div className="flex flex-col gap-[1rem] items-center">
-          <h1 className="font-sans font-semibold text-[3.5rem] leading-[120%] text-grayscale-950">
+          <h1 className="font-sans font-semibold text-[2rem] md:text-[3rem] lg:text-[3.5rem] leading-[120%] text-grayscale-950">
             The Custo Journal
           </h1>
-          <p className="text-xl-regular text-grayscale-600">
+          <p className="text-xl-regular text-[1rem] md:text-[1.125rem] text-grayscale-600 text-center">
             Get insights and news to to scale your business with confidence.
           </p>
         </div>
