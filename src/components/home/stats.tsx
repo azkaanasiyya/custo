a2a630c232bf7df5bad90df45372ff03e25ceca3
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-19 10:49:10 +0700

    fix(stats) : improve responsiveness of stats section and fetch data using dummy data

--- src/components/home/stats.tsx
+++ src/components/home/stats.tsx
@@ -1,49 +1,27 @@
+import { stats } from "../data/stats";
+
 export default function HomeStats() {
   return (
     <div className="flex w-full justify-center">
-      <div className="flex flex-row gap-[3rem] w-full max-w-[67rem] py-[4rem]">
-        <div className="flex flex-col gap-[1.25rem] items-center py-[2.5rem]">
-          <h2 className="font-sans font-semibold leading-[140%] text-[2.5rem] text-primary-500">
-            10K+
-          </h2>
-          <div className="flex flex-col gap-2 items-center">
-            <h4 className="font-sans font-semibold leading-[140%] text-[1.5rem]">
-              Users Worldwide
-            </h4>
-            <p className="text-m text-grayscale-600 text-center">
-              Businesses trust Custo to streamline sales and manage customer
-              relationships.
-            </p>
+      <div className="flex flex-row gap-[3rem] w-full max-w-mobile md:max-w-tablet lg:max-w-desktop py-[4rem]">
+        {stats.map((stat, index) => (
+          <div
+            className="flex flex-col gap-[1.25rem] items-center py-[2.5rem]"
+            key={index}
+          >
+            <h2 className="font-sans font-semibold leading-[140%] text-[1.75rem] md:text-[2.25rem] lg:text-[2.5rem] text-primary-500">
+              {stat.value}
+            </h2>
+            <div className="flex flex-col gap-2 items-center">
+              <h4 className="font-sans font-semibold leading-[140%] md:text-[1.25rem] lg:text-[1.5rem]">
+                {stat.title}
+              </h4>
+              <p className="text-m-regular text-[] md:text-[0.875rem] text-grayscale-600 text-center">
+                {stat.description}
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col gap-[1.25rem] items-center py-[2.5rem]">
-          <h2 className="font-sans font-semibold leading-[140%] text-[2.5rem] text-primary-500">
-            30%
-          </h2>
-          <div className="flex flex-col gap-2 items-center">
-            <h4 className="font-sans font-semibold leading-[140%] text-[1.5rem]">
-              Faster Sales Cycles
-            </h4>
-            <p className="text-m text-grayscale-600 text-center">
-              Optimize your sales process and close deals more efficiently with
-              automated workflows.
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-[1.25rem] items-center py-[2.5rem]">
-          <h2 className="font-sans font-semibold leading-[140%] text-[2.5rem] text-primary-500">
-            $1B
-          </h2>
-          <div className="flex flex-col gap-2 items-center">
-            <h4 className="font-sans font-semibold leading-[140%] text-[1.5rem]">
-              in Deals Closed
-            </h4>
-            <p className="text-m text-grayscale-600 text-center">
-              Track high-value opportunities and drive revenue growth
-              effortlessly.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
