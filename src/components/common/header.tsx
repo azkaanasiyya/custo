a7ef8da8cfcbd107f2bba5f763894ae0d78b635e
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-19 12:12:57 +0700

    fix(styles) : adjust font sizes for better responsive design

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -7,13 +7,11 @@ interface HeaderProps {
 export default function Header({ title, description, className = "" }: HeaderProps) {
     return (
         <div className={`flex flex-col justify-center items-center gap-4 ${className}`}>
-            <div className="typography font-semibold text-grayscale-950">
-                <h3 className="text-[28px] leading-[140%] md:text-[40px] md:leading-[120%] text-center">
+                <h2 className="font-sans text-[1.75rem] md:text-[2.25rem] lg:text-[2.5rem] font-semibold leading-[140%] text-center text-gray-950">
                     {title}
-                </h3>
-            </div>
+                </h2>
             {description && (
-                <p className="text-[16px] leading-[160%] text-grayscale-600 md:text-xlarge text-center">
+                <p className="text-[1rem] md:text-[1.125rem] leading-[160%] text-grayscale-600 text-center">
                     {description}
                 </p>
             )}
