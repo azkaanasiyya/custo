5315c6a439deb43a2c10886404d3a148383a3976
abidgymnastiar <abidgymnastiar@gmail.com>
2025-05-30 12:37:39 +0700

    fix(blog) : remove animation from blog menu title

--- src/components/blog/blogcontent.tsx
+++ src/components/blog/blogcontent.tsx
@@ -1,18 +1,11 @@
-import SlideFadeIn from "../animation/on-scroll/SlideFadeIn";
 import BlogList from "../common/bloglist";
 
 export default function BlogContent() {
   return (
-    <SlideFadeIn
-      items={[
-        { selector: "[data-animate='blog-1']", direction: "y", from: 100 },
-      ]}
-      staggerDelay={0.15}
-    >
+
       <div className="flex w-full justify-center">
         <div className="flex flex-col gap-[4rem] w-full max-w-mobile md:max-w-tablet lg:max-w-desktop py-[6.5rem]">
           <div
-            data-animate="blog-1"
             className="flex flex-col gap-[1rem] items-center"
           >
             <h1 className="font-sans font-semibold text-[2rem] md:text-[3rem] lg:text-[3.5rem] leading-[120%] text-grayscale-950">
@@ -25,6 +18,5 @@ export default function BlogContent() {
           <BlogList />
         </div>
       </div>
-    </SlideFadeIn>
   );
 }
