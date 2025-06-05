7b5073da5fc168f45c6a7a159ed8b24b0f8e082a
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-06-05 10:49:45 +0700

    refactor(animation): replace SlideFadeIn with FadeInSection in about page

--- src/components/about/hero.tsx
+++ src/components/about/hero.tsx
@@ -9,7 +9,7 @@ export default function AboutHero() {
       <div className="hero-section pt-[7.5rem] pb-20 px-6 rounded-[12px] md:pt-[9.5rem] md:pb-[6rem] md:px-[3.5rem] flex flex-col bg-grayscale-50 items-center">
         <div className="container w-full mx-auto max-w-[67rem] flex flex-col gap-16 items-center justify-center">
 
-          <FadeInSection stagger={0.3} className="w-full" variant="bottom-to-top">
+          <FadeInSection delay={0.5} className="w-full" variant="bottom-to-top">
             <div className="header flex flex-col gap-4 items-center">
               <h1 className="text-[32px] leading-[120%] font-semibold md:text-[56px] md:leading-[120%]">
                 About Custo
@@ -21,23 +21,23 @@ export default function AboutHero() {
             </div>
           </FadeInSection>
 
-          <FadeInSection delay={0} stagger={0.3} className="w-full" variant="bottom-to-top">
+          <div className="w-full">
             <div className="content grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[1.25rem] w-full">
-              <div className="flex flex-col gap-[1.25rem]">
+              <FadeInSection stagger={0.3} className="flex flex-col gap-[1.25rem]">
                 <Image src="content/content1.svg" alt="content" width={344} height={198} priority />
                 <Image src="content/content2.svg" alt="content" width={344} height={198} priority />
-              </div>
-              <div>
+              </FadeInSection>
+              <FadeInSection stagger={0.3}>
                 <Image src="content/content3.svg" alt="content" width={344} height={416} priority />
-              </div>
-              <div className="flex flex-col gap-[1.25rem] hidden lg:flex">
+              </FadeInSection>
+              <FadeInSection stagger={0.3} className="flex flex-col gap-[1.25rem] hidden lg:flex">
                 <Image src="content/content4.svg" alt="content" width={344} height={198} priority />
                 <Image src="content/content5.svg" alt="content" width={344} height={198} priority />
-              </div>
+              </FadeInSection>
             </div>
-          </FadeInSection>
+          </div>
 
-          <FadeInSection variant="bottom-to-top" className="w-full">
+          <FadeInSection stagger={0.3} variant="bottom-to-top" className="w-full">
             <div className="clients flex flex-col gap-8 pt-[0.5rem] justify-center items-center w-full overflow-hidden">
               <p className="text-[16px] leading-[160%] max-w-[41rem] text-center text-grayscale-600 md:text-xlarge">
                 Trusted by companies around the world
