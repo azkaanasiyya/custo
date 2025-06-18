58309bfd52f580ccf4113ac13c2f888c5b3e6123
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-06-18 10:13:58 +0700

    style(ui): adjust close icon size and center alignment

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -69,9 +69,9 @@ function DialogContent({
         {showCloseButton && (
           <DialogPrimitive.Close
             data-slot="dialog-close"
-            className="ring-offset-background focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-70 transition-opacity hover:opacity-100 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
+            className="flex items-center justify-center cursor-pointer focus:ring-ring data-[state=open]:bg-accent data-[state=open]:text-muted-foreground absolute top-4 right-4 w-12 h-12 rounded-full bg-[#FFFFFF66] transition-opacity hover:opacity-100 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-6"
           >
-            <XIcon />
+            <XIcon className="size-6 text-white" />
             <span className="sr-only">Close</span>
           </DialogPrimitive.Close>
         )}
