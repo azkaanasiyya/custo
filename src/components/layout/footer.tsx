15dbb874e34c42096d738469ac446e0c73ccf2b3
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-06-20 13:43:30 +0700

    style(home): update font size on cta and footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -10,7 +10,7 @@ export default function Footer() {
                         <FadeInSection delay={0.5} className="top-left flex flex-col gap-6">
                             <div className="flex flex-col gap-3 max-w-[246px] lg:max-w-[16rem]">
                                 <Image src="/logo/logo-footer.svg" alt="footer" width={92} height={24} />
-                                <p className="text-medium text-white opacity-[72%]">Simplify customer relationships and scale your business with Custo.</p>
+                                <p className="text-[12px] md:text-medium text-white opacity-[72%]">Simplify customer relationships and scale your business with Custo.</p>
                             </div>
                             <div className="flex flex-row gap-2">
                                 <a href="#">
@@ -29,8 +29,8 @@ export default function Footer() {
                         </FadeInSection>
                         <FadeInSection delay={0.5} className="top-right grid grid-cols-2 gap-10 md:gap-0 md:grid-cols-3 justify-between md:min-w-[20.9rem] lg:min-w-0 lg:gap-[7.5rem]">
                             <div className="flex flex-col gap-4">
-                                <p className="text-large text-white">Quick Links</p>
-                                <ul className="text-medium text-white opacity-[72%] space-y-3">
+                                <p className="text-[14px] font-medium md:text-large text-white">Quick Links</p>
+                                <ul className="text-[12px] md:text-medium text-white opacity-[72%] space-y-3">
                                     <li><a href="#">Benefits</a></li>
                                     <li><a href="#">Features</a></li>
                                     <li><a href="#">Testimonials</a></li>
@@ -38,8 +38,8 @@ export default function Footer() {
                                 </ul>
                             </div>
                             <div className="flex flex-col gap-4">
-                                <p className="text-large text-white">Main Pages</p>
-                                <ul className="text-medium text-white opacity-[72%] space-y-3">
+                                <p className="text-[14px] font-medium md:text-large text-white">Main Pages</p>
+                                <ul className="text-[12px] md:text-medium text-white opacity-[72%] space-y-3">
                                     <li><a href="/home">Home</a></li>
                                     <li><a href="/about">About</a></li>
                                     <li><a href="/pricing">Pricing</a></li>
@@ -47,8 +47,8 @@ export default function Footer() {
                                 </ul>
                             </div>
                             <div className="flex flex-col gap-4">
-                                <p className="text-large text-white">Other Pages</p>
-                                <ul className="text-medium text-white opacity-[72%] space-y-3">
+                                <p className="text-[14px] font-medium md:text-large text-white">Other Pages</p>
+                                <ul className="text-[12px] md:text-medium text-white opacity-[72%] space-y-3">
                                     <li>
                                         <Link href="/blog">
                                             Blog
@@ -61,9 +61,9 @@ export default function Footer() {
                         </FadeInSection>
                     </div>
                     <FadeInSection delay={0.5} className="bottom flex flex-row py-6 justify-between border-t border-white/12">
-                        <p className="text-medium font-normal text-white">2025 © Custo</p>
+                        <p className="text-[12px] md:text-medium font-normal text-white">2025 © Custo</p>
                         <div className="flex flex-row gap-2">
-                            <a href="#" className="text-medium font-normal text-white">Get this template</a>
+                            <a href="#" className="text-[12px] md:text-medium font-normal text-white">Get this template</a>
                             <Image src="/icon/arrow.svg" alt="arrow" width={12} height={12} />
                         </div>
                     </FadeInSection>
