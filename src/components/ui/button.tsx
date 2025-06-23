e4dcd03d6ab92dff7682d1e56b4d47eed357761c
Azka Anasiyya Haris <116702442+azkaanasiyya@users.noreply.github.com>
2025-06-23 16:32:59 +0700

    fix(navbar): add overlay behind mobile menu

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
       size: {
         base: "px-[24px] py-[11px] font-sans font-medium text-[14px] md:text-[1rem] rounded-[80px]",
         small:
-          "px-[20px] py-[9px] font-sans font-medium text-[0.875rem] rounded-[80px]",
+          "px-[20px] py-[9px] font-sans font-medium text-[12px] md:text-[0.875rem] rounded-[80px]",
         icon: "size-9",
         testimonial: 
           "w-[48px] h-[48px] rounded-full",
